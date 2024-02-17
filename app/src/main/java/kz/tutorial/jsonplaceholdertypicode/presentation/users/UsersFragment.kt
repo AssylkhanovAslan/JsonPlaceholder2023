3f4fbaaf204b545fed3785225eefebe4b7fd0413
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2024-02-17 17:35:28 +0600

    Updated instructions

--- app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/users/UsersFragment.kt
+++ app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/users/UsersFragment.kt
@@ -4,5 +4,14 @@ import androidx.fragment.app.Fragment
 import kz.tutorial.jsonplaceholdertypicode.R
 
 class UsersFragment : Fragment(R.layout.fragment_users) {
-
+    /*
+        TODO: По сути тут ничего сложного.
+         Здесь вам просто нужно запустить запрос на https://jsonplaceholder.typicode.com/users
+         Моделька у вас уже есть
+         Дальше вам нужно просто создать экран отображения пользователей по фигме и нашей архитектуре
+        Рекомендую идти снизу вверх по древу зависимостей:
+        Запрос, моделька у вас уже есть, метод в репо есть
+        Осталось сделать GetAllUsersUseCase, ViewModel и потом UI
+        Также, не забываем про клик на имейл
+     */
 }
